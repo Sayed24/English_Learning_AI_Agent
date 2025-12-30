dac61d7de5c922bd8554d694c5e5e91f23b3438e
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-31 00:49:19 +0300

    Update certificate.js

--- certificate.js
+++ certificate.js
@@ -1,1 +1,8 @@
+function generateCertificate() {
+  if (progress.day < 30) {
+    alert("Complete all 30 days first!");
+    return;
+  }
+  alert("🎉 Certificate earned!\nEnglish Course Completed!");
+}
 
