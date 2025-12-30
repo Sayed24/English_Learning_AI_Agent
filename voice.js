6fabc24a8c578450e1712f17d77b8a18a8c41e6e
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-31 00:47:32 +0300

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,1 +1,14 @@
+function startVoice() {
+  const rec = new webkitSpeechRecognition();
+  rec.lang = "en-US";
+  rec.onresult = e => {
+    document.getElementById("userInput").value = e.results[0][0].transcript;
+  };
+  rec.start();
+}
+
+function speak(text) {
+  const msg = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(msg);
+}
 
